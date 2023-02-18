95aa1bb0bf2fd4b51ecb20c08e1145bb6abbfa15
shazilmemon <shazilmemon6@gmail.com>
2023-02-18 03:26:43 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function mouseover() {
+    var mouse = document.getElementById('mouse')
+    .style.backgroundColor = ('yellow');
+}
+
+function mouseout() {
+    var mouse = document.getElementById('mouse')
+    .style.backgroundColor = ('blue');
+    
+}
+
+
